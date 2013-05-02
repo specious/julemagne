9d8dd2f6b814b846ebb176f31ab6fd3a2a717504
Ildar Sagdejev <specious@gmail.com>
2013-05-02 15:37:47 -0400

    Carousel showcase with 6 thumbnails

---
+++ js/site.js
@@ -0,0 +1,25 @@
+/* Aloha! */
+
+$(function() {
+	var showcase = $("#showcase");
+
+	showcase.CloudCarousel({			
+		xPos: 980/2,
+		yPos: 50,
+		yRadius: 480 / 10,
+		reflHeight: 56,
+		reflGap: 2,
+		speed: 0.18,
+		buttonLeft: $("#nav-left"),
+		buttonRight: $("#nav-right"),
+		titleBox: $("#art-title"),
+		altBox: $("#art-alt")
+	});
+
+	// CloudCarousel messes it up on init
+	$(".nav-button").css('display', 'inline-block');
+
+	showcase.css('visibility', 'visible');
+	showcase.css('display', 'none');
+	showcase.fadeIn(1000, 'swing');
+})
